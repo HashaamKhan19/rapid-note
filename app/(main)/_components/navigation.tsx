6f45a5ee8a466fff86c0053c3d5eaf257ef829de
hashaam <hashaamkhan4247@gmail.com>
2023-11-29 23:24:27 +0500

    did many things in the sidebar idk

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -24,18 +24,21 @@ import {
 } from "@/components/ui/popover";
 import { useSearch } from "@/hooks/use-search";
 import { useSettings } from "@/hooks/use-settings";
-import { UserItem } from "./UserItem";
 
-// import { UserItem } from "./user-item";
-// import { Item } from "./item";
-// import { DocumentList } from "./document-list";
+import { UserItem } from "./user-item";
+import { Item } from "./item";
+import { DocumentList } from "./document-list";
 // import { TrashBox } from "./trash-box";
-// import { Navbar } from "./navbar";
+import { Navbar } from "./navbar";
 
 export const Navigation = () => {
+  const router = useRouter();
+  const settings = useSettings();
+  const search = useSearch();
+  const params = useParams();
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
-  //   const create = useMutation(api.documents.create);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -118,16 +121,17 @@ export const Navigation = () => {
     }
   };
 
-  //   const handleCreate = () => {
-  //     const promise = create({ title: "Untitled" })
-  //       .then((documentId) => router.push(`/documents/${documentId}`))
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" }).then((documentId) =>
+      router.push(`/documents/${documentId}`)
+    );
 
-  //     toast.promise(promise, {
-  //       loading: "Creating a new note...",
-  //       success: "New note created!",
-  //       error: "Failed to create a new note."
-  //     });
-  //   };
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note.",
+    });
+  };
 
   return (
     <>
@@ -151,8 +155,26 @@ export const Navigation = () => {
         </div>
         <div>
           <UserItem />
+          <Item label="Search" icon={Search} isSearch onClick={search.onOpen} />
+          <Item label="Settings" icon={Settings} onClick={settings.onOpen} />
+          <Item onClick={handleCreate} label="New page" icon={PlusCircle} />
         </div>
-        <div className="mt-4">Documents</div>
+        <div className="mt-4">
+          <DocumentList />
+          <Item onClick={handleCreate} icon={Plus} label="Add a page" />
+          <Popover>
+            <PopoverTrigger className="w-full mt-4">
+              <Item label="Trash" icon={Trash} />
+            </PopoverTrigger>
+            <PopoverContent
+              className="p-0 w-72"
+              side={isMobile ? "bottom" : "right"}
+            >
+              {/* <TrashBox /> */}
+              TrashBox here
+            </PopoverContent>
+          </Popover>
+        </div>
         <div
           onMouseDown={handleMouseDown}
           onClick={resetWidth}
@@ -167,15 +189,19 @@ export const Navigation = () => {
           isMobile && "left-0 w-full"
         )}
       >
-        <nav className="bg-transparent px-3 py-2 w-full">
-          {isCollapsed && (
-            <MenuIcon
-              onClick={resetWidth}
-              role="button"
-              className="h-6 w-6 text-muted-foreground"
-            />
-          )}
-        </nav>
+        {!!params.documentId ? (
+          <Navbar isCollapsed={isCollapsed} onResetWidth={resetWidth} />
+        ) : (
+          <nav className="bg-transparent px-3 py-2 w-full">
+            {isCollapsed && (
+              <MenuIcon
+                onClick={resetWidth}
+                role="button"
+                className="h-6 w-6 text-muted-foreground"
+              />
+            )}
+          </nav>
+        )}
       </div>
     </>
   );
