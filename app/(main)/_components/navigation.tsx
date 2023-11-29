6cb785e534e1353132295a5aba588fb588dde49b
hashaam <hashaamkhan4247@gmail.com>
2023-11-29 23:48:31 +0500

    feat:trashbox and revert

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -28,7 +28,7 @@ import { useSettings } from "@/hooks/use-settings";
 import { UserItem } from "./user-item";
 import { Item } from "./item";
 import { DocumentList } from "./document-list";
-// import { TrashBox } from "./trash-box";
+import { TrashBox } from "./trash-box";
 import { Navbar } from "./navbar";
 
 export const Navigation = () => {
@@ -170,8 +170,7 @@ export const Navigation = () => {
               className="p-0 w-72"
               side={isMobile ? "bottom" : "right"}
             >
-              {/* <TrashBox /> */}
-              TrashBox here
+              <TrashBox />
             </PopoverContent>
           </Popover>
         </div>
